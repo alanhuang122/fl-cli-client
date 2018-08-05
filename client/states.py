ed509a1a5c28ec8501fac791c1a01ab3c2da2373
Alan Huang <Alan.Huang@utdallas.edu>
2018-08-04 19:33:04 -0500

    Add function to enumerate and switch states

--- client/states.py
+++ client/states.py
@@ -9,8 +9,20 @@ class State(Enum):
     Fate = 5
     Plans = 6
 
-def clean(text):
-    tag_re = re.compile(r'(<!--.*?-->|<[^>]*>)')
+def choose_state():
+    states = list(State)
+    for i in range(len(states)):
+        print('{}: {}'.format(i+1, str(states[i]).split('.')[1]))
+    while True:
+        try:
+            choice = int(input('Type the number of the tab you want to switch to: '))
+            if choice > len(states) or choice < 1:
+                print("Invalid input.")
+                continue
+            return State(choice - 1)
+        except:
+            print("Invalid input.")
+            continue
 
     # Remove well-formed tags, fixing mistakes by legitimate users
     clean = tag_re.sub('', text)
