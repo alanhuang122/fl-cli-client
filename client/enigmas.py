c104e92b54b92c3e867895648ef2e82dc3807763
Alan Huang <Alan.Huang@utdallas.edu>
2018-08-29 15:57:50 -0500

    Add automation script that uses the API bindings

---
+++ client/enigmas.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+from datetime import datetime
+import os
+import random
+import sys
+import time
+
+from character import Character
+
+print(f'current time {datetime.now()}')
+
+delay = random.randint(0, 30*60)
+
+print(f'sleeping for {delay} secs...')
+
+sys.stdout.flush()
+
+#time.sleep(delay)
+
+c = Character()
+
+assert c.get_phase() == 'In' and c.status['storylet']['id'] == 284781
+
+actions = c.get_actions()
+
+print('actions: {}/{}'.format(actions[0], actions[1]))
+
+for x in range(actions[0] // 4):
+    c.cb(211145)
+    c.pr()
+
+print('done')
